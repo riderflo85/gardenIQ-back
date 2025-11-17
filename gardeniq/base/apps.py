1b089b7d985d5dd156a28794029c86f10c8f7abb
Florent (Mac) <grenaille.florent85@gmail.com>
2025-11-17 14:01:22 +0100

    (TK-41)(feature) - Update BaseConfig class to import versions when running application

--- gardeniq/base/apps.py
+++ gardeniq/base/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gardeniq.base"
+
+    def ready(self) -> None:
+        # Import versions this to ensure that requests to many github repot have been successfully completed.
+        from gardeniq.__version__ import __version__  # noqa
+        from gardeniq.__version__ import garden_firmware_version  # noqa
+        from gardeniq.__version__ import micropython_version  # noqa

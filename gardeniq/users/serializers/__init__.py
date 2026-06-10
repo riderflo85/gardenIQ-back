0d14301a489b448ca3426c458e11a5195aa08b0d
Ryzhy44 <bandit44000@gmail.com>
2026-06-11 01:22:18 +0200

    (TK-38) - (refactor) - Replace group and permission serializers with their read-only versions

--- gardeniq/users/serializers/__init__.py
+++ gardeniq/users/serializers/__init__.py
@@ -1,5 +1,5 @@
-from .users import GroupSerializer
-from .users import PermissionSerializer
+from .users import GroupReadOnlySerializer
+from .users import PermissionReadOnlySerializer
 from .users import UserDetailReadOnlySerializer
 from .users import UserReadOnlySerializer
 from .users import UserSerializer
